d590f4ac3da8aaa13144ee4d20b16aa60a0b25ec
jiren424 <junioratchade07@gmail.com>
2025-11-12 01:20:48 +0100

    feat(web): add routing and basic structure for clients

---
+++ react-app/src/clients/pages/ClientsPage.tsx
@@ -0,0 +1,11 @@
+
+import { Typography } from 'antd';
+
+export function ClientsPage() {
+  return (
+    <div>
+      <Typography.Title level={1}>Liste des Clients</Typography.Title>
+      
+    </div>
+  );
+}
